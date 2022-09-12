28638672c70387c98352b14abd7c658e040ca28a
Floyd-Furtado <113371065+Floyd-Furtado@users.noreply.github.com>
2022-09-12 08:41:45 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+pizza_list_array = [];
+
+function getMenu() {
+    var item = document.getElementById("pizza_name").value; 
+    pizza_list_array.push(item);
+    var remove_commas = pizza_list_array.join("<br># ");
+    document.getElementById("display_menu").innerHTML =  "# " + remove_commas;
+}
