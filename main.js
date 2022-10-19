89e063723595e3b642a8308ccf68dc394eae2b2d
Floyd Furtado <113371065+Floyd-Furtado@users.noreply.github.com>
2022-10-19 07:36:06 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-pizza_list_array = [];
+menu_list_array = [ "Veg Margherita Pizza",
+                    "Chicken Tandorri Pizza",
+                    "Veg Supreme Pizza",
+                    "Panner Tikka Pizza",
+                    "Deluxe Veggie Pizza",
+                    "Veg Extravaganza Pizza",
+                    "Pepperoni Pizza", 
+                    "Hawaiian Pizza",
+                    "Cheese Pizza",
+                    "BBQ Chicken Pizza",
+                    "Chicago Pizza"]; //Pizza item names
 
-function getMenu() {
-    var item = document.getElementById("pizza_name").value; 
-    pizza_list_array.push(item);
-    var remove_commas = pizza_list_array.join("<br># ");
-    document.getElementById("display_menu").innerHTML =  "# " + remove_commas;
+function getmenu()
+{
+    var htmldata="";
+    for(var i=0; i<menu_list_array.length; i++)
+    {
+        htmldata=htmldata+ menu_list_array[i] + '<br>'
+    }
+    
+    document.getElementById("display_menu").innerHTML = htmldata;
 }
+
+function add_suggestion(){
+	var item = document.getElementById("add_item").value;
+    
+    menu_list_array.push(item);
+}
+
+
+
